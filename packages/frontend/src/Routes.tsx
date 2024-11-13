c90d8a565120eab1250e046b893ede1a08adb048
Daníel Adams <me@dandabs.io>
2024-11-13 19:46:08 +0000

    Implemented secured pages

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import Login from "./containers/Login.tsx";
+import AuthenticatedRoute from "./components/AuthenticatedRoute.tsx";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute.tsx";
 
 export default function Links() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<AuthenticatedRoute><Home /></AuthenticatedRoute>} />
+      <Route path="/login" element={<UnauthenticatedRoute><Login /></UnauthenticatedRoute>} />
     </Routes>
   );
 }
