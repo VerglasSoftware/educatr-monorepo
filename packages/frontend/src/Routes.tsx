33f71f34196832b167fe448474c63c5a50f37d87
Jake Gillman <jakegillman3@gmail.com>
2024-11-13 23:03:56 +0000

    prettier

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,28 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./containers/Home.tsx";
-import Login from "./containers/Login.tsx";
 import AuthenticatedRoute from "./components/AuthenticatedRoute.tsx";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute.tsx";
+import Home from "./containers/Home.tsx";
+import Login from "./containers/Login.tsx";
 
 export default function Links() {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <AuthenticatedRoute>
-            <Home />
-          </AuthenticatedRoute>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <UnauthenticatedRoute>
-            <Login />
-          </UnauthenticatedRoute>
-        }
-      />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					<AuthenticatedRoute>
+						<Home />
+					</AuthenticatedRoute>
+				}
+			/>
+			<Route
+				path="/login"
+				element={
+					<UnauthenticatedRoute>
+						<Login />
+					</UnauthenticatedRoute>
+				}
+			/>
+		</Routes>
+	);
 }
