d24829ac623c3104e81a53ab42bdd29667e19398
Daníel Adams <me@dandabs.io>
2024-11-13 19:10:03 +0000

    Installed AWS Amplify

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,7 +1,28 @@
+import { Amplify } from "aws-amplify";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import config from "./config.ts";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "api",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
