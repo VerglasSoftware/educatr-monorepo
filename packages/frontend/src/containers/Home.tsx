8e08414a37dcb4bd3ce4946832c778aa10df8050
Daníel Adams <me@dandabs.io>
2024-11-14 10:13:22 +0000

    EDU-1 html title

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -1,8 +1,12 @@
 import "./Home.css";
+import {Helmet} from "react-helmet";
 
 export default function Home() {
   return (
     <div className="Home">
+        <Helmet>
+            <title>Home</title>
+        </Helmet>
       <div className="lander">
         <h1>Educatr</h1>
         <p className="text-muted">Hello, world!</p>
