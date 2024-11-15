8bc9414b0017f36b028cea02071b3e7f2d0b2dec
Jake Gillman <jakegillman3@gmail.com>
2024-11-15 15:37:08 +0000

    implemented organisation details without students

--- infra/api.ts
+++ infra/api.ts
@@ -1,4 +1,4 @@
-import { packTable, userTable } from "./storage";
+import { organisationTable, packTable } from "./storage";
 
 export const api = new sst.aws.ApiGatewayV2("Api", {
 	domain: $app.stage === "prod" ? "api.educatr.uk" : undefined,
@@ -8,7 +8,7 @@ export const api = new sst.aws.ApiGatewayV2("Api", {
 				auth: { iam: true },
 			},
 			handler: {
-				link: [packTable],
+				link: [packTable, organisationTable],
 			},
 		},
 	},
@@ -24,12 +24,28 @@ api.route("GET /pack", {
 api.route("POST /pack", {
 	handler: "packages/functions/src/pack.create",
 });
-api.route("GET /pack/{id}", {
+api.route("GET /pack/{packId}", {
 	handler: "packages/functions/src/pack.get",
 });
-api.route("PUT /pack/{id}", {
+api.route("PUT /pack/{packId}", {
 	handler: "packages/functions/src/pack.update",
 });
-api.route("DELETE /pack/{id}", {
+api.route("DELETE /pack/{packId}", {
 	handler: "packages/functions/src/pack.del",
+});
+
+api.route("GET /organisation", {
+	handler: "packages/functions/src/organisation.list",
+});
+api.route("POST /organisation", {
+	handler: "packages/functions/src/organisation.create",
+});
+api.route("GET /organisation/{id}", {
+	handler: "packages/functions/src/organisation.get",
+});
+api.route("PUT /organisation/{id}", {
+	handler: "packages/functions/src/organisation.update",
+});
+api.route("DELETE /organisation/{id}", {
+	handler: "packages/functions/src/organisation.del",
 });
