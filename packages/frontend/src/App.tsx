4e2d736807b12e39667b307e0e3392a55d5ba4a1
Daníel Adams <me@dandabs.io>
2024-11-25 10:54:40 +0000

    EDU-42 particles

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -8,17 +8,72 @@ import { Box, CssBaseline, CssVarsProvider, extendTheme } from "@mui/joy";
 import { Helmet } from "react-helmet";
 import { useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar.tsx";
+import { type ISourceOptions } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [init, setInit] = useState(false);
 
 	useEffect(() => {
 		onLoad();
 	}, []);
 
+  useEffect(() => {
+		initParticlesEngine(async (engine) => {
+			await loadFull(engine);
+		})
+			.then(() => {
+				setInit(true);
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	}, []);
+
+  const options: ISourceOptions = {
+		particles: {
+			number: {
+				value: 100,
+			},
+			color: {
+				value: "#ffffff",
+			},
+			links: {
+				enable: true,
+				distance: 200,
+        opacity: 0.5,
+			},
+			shape: {
+				type: "circle",
+			},
+			opacity: {
+				value: 0.6,
+			},
+			size: {
+				value: {
+					min: 4,
+					max: 6,
+				},
+			},
+			move: {
+				enable: true,
+				speed: 1,
+			},
+		},
+		background: {
+			color: "#1c1851",
+		},
+		poisson: {
+			enable: true,
+		},
+	};
+
   const location = useLocation();
   const showSidebar = location.pathname.startsWith("/dash");
+  const inPlayMode = location.pathname.startsWith("/play") || location.pathname.startsWith("/launch");
 
   const theme = extendTheme({
     fontFamily: {
@@ -44,14 +99,32 @@ function App() {
     !isAuthenticating && (
       <div className="App">
         <Helmet titleTemplate="%s - Educatr">
-            <title>My Title</title>
+            <title></title>
         </Helmet>
         <AppContext.Provider
           value={{ isAuthenticated, userHasAuthenticated } as AppContextType}
         >
           <CssVarsProvider disableTransitionOnChange theme={theme}>
             <CssBaseline />
-            <Box
+            {
+              inPlayMode ? (
+                <Box sx={{
+                  minHeight: '100vh',
+                }}>
+                  <style>{` body {background-color: #1c1851;} `}</style>
+                  {init && (
+                    <Particles
+                      id="particles"
+                      options={options}
+                    />
+                  )}
+                  <main>
+                    <Routes />
+                  </main>
+                </Box>
+                
+              ) : (
+                <Box
             sx={[
               {
                 display: 'grid',
@@ -71,6 +144,8 @@ function App() {
                 <Routes />
               </main>
             </Box>
+              )
+            }
           </CssVarsProvider>
         </AppContext.Provider>
       </div>
