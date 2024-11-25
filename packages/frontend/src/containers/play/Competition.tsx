8b9aa8ba20ebd2cce4ecc18e1bc4901d0c7ca76e
Daníel Adams <me@dandabs.io>
2024-11-25 17:44:08 +0000

    EDU-42 loading screen

--- packages/frontend/src/containers/play/Competition.tsx
+++ packages/frontend/src/containers/play/Competition.tsx
@@ -5,9 +5,11 @@ import { useEffect, useState } from "react";
 import { API } from "aws-amplify";
 import { useParams } from "react-router-dom";
 import NavbarMain from "../../components/play/Navbar";
+import { DotWave } from "@uiball/loaders";
 
 export default function PlayCompetition() {
     const [competition, setCompetition] = useState<any>();
+    const [packs, setPacks] = useState<any[]>();
 
     const { compId } = useParams();
 
@@ -16,6 +18,9 @@ export default function PlayCompetition() {
             try {
                 const competition = await API.get("api", `/competition/${compId}`, {});
                 setCompetition(competition);
+
+                const packs = await API.get("api", `/pack`, {});
+                setPacks(packs);
             } catch (e) {
                 console.log(e);
             }
@@ -24,7 +29,38 @@ export default function PlayCompetition() {
         onLoad();
     }, []);
 
-  return competition && (
+    if (!competition || !packs) {
+        return (
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '85vh',
+                flexDirection: 'column',
+                overflow: 'hidden'
+            }}>
+            <Card variant="plain" sx={{ backgroundColor: 'rgb(0 0 0 / 0.3)', width: '60%' }}>
+            <CardContent sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '2%'
+            }}>
+
+                    <DotWave color="#FFF" />
+
+                <Typography level="title-lg" textColor="common.white" sx={{ mt: 2 }}>Getting ready</Typography>
+                { !competition && <Typography level="body-sm" textColor="common.white">Downloading competition data</Typography> }
+                { !packs && <Typography level="body-sm" textColor="common.white">Downloading pack data</Typography> }
+
+            </CardContent>
+      </Card>
+        </Box>
+        )
+    }
+
+  return (
     <div className="Home">
         <Helmet>
             <title>{competition.name}</title>
@@ -40,7 +76,15 @@ export default function PlayCompetition() {
             padding: '2%'
         }}>
 
-            <Typography level="h2" component="h1" textColor="common.white">{competition.name}</Typography>
+            
+
+            <Stack spacing={2} sx={{ width: '100%' }}>
+                {packs.map((pack) => (
+                    <>
+                    <Typography level="h2" component="h1" textColor="common.white">{pack.name.S}</Typography>
+                    </>
+                ))}
+            </Stack>
 
         </Box>
 
